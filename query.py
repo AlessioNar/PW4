ffb7978344be6b8708b3fee25eb21248fa4be285
AlessioNar <alessio.nardin@gmail.com>
2023-09-11 21:19:41 +0200

    finished, done with cleaning

--- query.py
+++ query.py
@@ -7,41 +7,57 @@ import pandas as pd
 namespaces = {
     'rdf': "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
     'rdfs': "http://www.w3.org/2000/01/rdf-schema#",
-    'schema': "http://schema.org/",
+    'schema1': "http://schema.org/",
     'gr': "http://purl.org/goodrelations/v1#",
     'foaf': "http://xmlns.com/foaf/0.1/",
-    'ben': "http://benettondata.it/graph"
+    'ben': "http://benettondata.it/graph/"
 }
 
 # Define RDF graph
 g = Graph()
 g.bind('rdf', RDF)
 g.bind('rdfs', RDFS)
-g.bind('schema', Namespace(namespaces['schema']))
+g.bind('schema1', Namespace(namespaces['schema1']))
 g.bind('gr', Namespace(namespaces['gr']))
 g.bind('foaf', Namespace(namespaces['foaf']))
 g.bind('ben', Namespace(namespaces['ben']))
 
 # Load RDF data from turtle file
-g.parse('benettonsuppliers.ttl', format='turtle')
+g.parse('data/benettonsuppliers.ttl', format='turtle')
+# Define query descriptions
+query_descriptions = {
+    'query1': "Count Companies with Wet Process and Certificates",
+    'query2': "In how many countries does the Benetton Group supply from?",
+    'query3': "Calculate Averages of Men, Women, and Migrant Percentages by Country",
+    'query4': "List Company Names with Collective Bargaining Agreements (CBA) and Coverage Percentages"
+}
 
 # Prepare SPARQL queries
 queries = {
     'query1': prepareQuery("""
-        SELECT (COUNT(?company) AS ?count) WHERE {
-          ?company a gr:BusinessEntity.
-          ?company ben:hasProcess ben:WetProcess.
-          ?company ben:hasCertificate ?cert.
-        }
+    
+   SELECT (COUNT(?company) AS ?count) WHERE {
+    ?company a gr:BusinessEntity.
+    ?company ben:hasProcess ben:Manufacturing.
+    ?company ben:hasCertificate ?certificate.
+    FILTER(?certificate != "")
+}
     """, initNs=namespaces),
+
     'query2': prepareQuery("""
-        SELECT ?companyName ?tradeUnionName WHERE {
-          ?company a gr:BusinessEntity.
-          ?company ben:hasTradeUnion true.
-          ?company ben:tradeUnionName ?tradeUnionName.
-          ?company ben:name ?companyName.
+       SELECT (COUNT(DISTINCT ?country) AS ?count)
+        WHERE {
+        ?company a gr:BusinessEntity.
+        ?company ben:hasProcess ben:WetProcess.
+        ?company ben:hasCertificate ?cert.
+        ?company ben:hasProductType ben:Apparel.
+        ?company ?addressProp ?address.
+        ?address schema1:addressCountry ?country.
+        FILTER(?addressProp = schema1:address)
         }
+
     """, initNs=namespaces),
+
     'query3': prepareQuery("""
         SELECT ?country (AVG(?menPercentage) AS ?avgMen) (AVG(?womenPercentage) AS ?avgWomen) (AVG(?migrantPercentage) AS ?avgMigrants) WHERE {
           ?company a gr:BusinessEntity.
@@ -53,6 +69,7 @@ queries = {
         }
         GROUP BY ?country
     """, initNs=namespaces),
+
     'query4': prepareQuery("""
         SELECT ?companyName ?cbaPercentage WHERE {
           ?company a gr:BusinessEntity.
@@ -63,9 +80,10 @@ queries = {
     """, initNs=namespaces),
 }
 
-# Execute and print query results
+# Execute and print query descriptions and results
 for query_name, query in queries.items():
-    print(f"Query: {query_name}")
+    print(f"{query_descriptions[query_name]}")    
+
     results = g.query(query)
     for row in results:
         print(row)
